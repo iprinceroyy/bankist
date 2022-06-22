79aa671e2c5d2577687d83214d42b1ce8d82210a
Prince Roy <princeku.ray143@gmail.com>
2022-06-22 17:19:50 +0530

    update script.js

--- script.js
+++ script.js
@@ -559,3 +559,61 @@ btnSort.addEventListener('click', function(e) {
 
 // console.log(convertToTitleCase('priNce roy is Very smart boy'));
 // console.log(convertToTitleCase('and here is a good example'));
+
+// Coding challenge 4
+
+const dogs = [
+    { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+    { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+    { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+
+// 1
+dogs.forEach(
+    dog => (dog.recommendedFood = Math.trunc(dog.weight ** 0.75 * 28))
+);
+console.log(dogs);
+
+// 2
+const dogSarah = dogs.find(dog => dog.owners.includes('Sarah'));
+console.log(dogSarah);
+
+console.log(
+    `Sarah's dog is eating too${
+    dogSarah.curFood > dogSarah.recommendedFood ? 'much' : 'little'
+  }`
+);
+
+// 3
+const ownersEatTooMuch = dogs
+    .filter(dog => dog.curFood > dog.recommendedFood)
+    .flatMap(dog => dog.owners);
+console.log(ownersEatTooMuch);
+
+const ownersEatTooLittle = dogs
+    .filter(dog => dog.curFood < dog.recommendedFood)
+    .flatMap(dog => dog.owners);
+console.log(ownersEatTooLittle);
+
+// 4
+console.log(`${ownersEatTooMuch.join(' and ')}'s dogs eat too much!`);
+console.log(`${ownersEatTooLittle.join(' and ')}'s dogs eat too little!`);
+
+// 5
+console.log(dogs.some(dog => dog.curFood === dog.recommendedFood));
+
+// 6
+const checkEatingOkay = dog =>
+    dog.curFood > dog.recommendedFood * 0.9 &&
+    dog.curFood < dog.recommendedFood * 1.1;
+
+console.log(dogs.some(checkEatingOkay));
+
+// 7
+console.log(dogs.filter(checkEatingOkay));
+
+// 8
+const dogsSorted = dogs
+    .slice()
+    .sort((a, b) => a.recommendedFood - b.recommendedFood);
+console.log(dogsSorted);
