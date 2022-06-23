f1d61e4fb8be1aefbbe1cf098c959ba240d40052
Prince Roy <princeku.ray143@gmail.com>
2022-06-23 22:34:27 +0530

    update script.js

--- script.js
+++ script.js
@@ -18,9 +18,9 @@ const account1 = {
         '2020-01-28T09:15:04.904Z',
         '2020-04-01T10:17:24.185Z',
         '2020-05-08T14:11:59.604Z',
-        '2020-05-27T17:01:17.194Z',
-        '2020-07-11T23:36:17.929Z',
-        '2020-07-12T10:51:36.790Z',
+        '2022-06-25T17:01:17.194Z',
+        '2022-06-24T23:36:17.929Z',
+        '2022-06-23T10:51:36.790Z',
     ],
     currency: 'EUR',
     locale: 'pt-PT', // de-DE
@@ -38,9 +38,9 @@ const account2 = {
         '2019-12-25T06:04:23.907Z',
         '2020-01-25T14:18:46.235Z',
         '2020-02-05T16:33:06.386Z',
-        '2020-04-10T14:43:26.374Z',
-        '2020-06-25T18:49:59.371Z',
-        '2020-07-26T12:01:20.894Z',
+        '2020-06-25T14:43:26.374Z',
+        '2020-06-24T18:49:59.371Z',
+        '2022-06-23T12:01:20.894Z',
     ],
     currency: 'USD',
     locale: 'en-US',
@@ -74,6 +74,27 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+/////////////////////////////////////
+// Functions
+
+const formatMovementDate = function(date) {
+    const calcDaysPassed = (date1, date2) =>
+        Math.round(Math.abs(date1 - date2) / (1000 * 60 * 60 * 24));
+
+    const daysPassed = calcDaysPassed(new Date(), date);
+    console.log(daysPassed);
+
+    if (daysPassed === 0) return 'Today';
+    if (daysPassed === 1) return 'Yesterday';
+    if (daysPassed <= 7) return `${daysPassed} days ago`;
+    else {
+        const day = `${date.getDate()}`.padStart(2, 0);
+        const month = `${date.getMonth() + 1}`.padStart(2, 0);
+        const year = date.getFullYear();
+        return `${day}/${month}/${year}`;
+    }
+};
+
 const displayMovements = function(acc, sort = false) {
     containerMovements.innerHTML = ''; //to clear data displayed previously
 
@@ -85,10 +106,7 @@ const displayMovements = function(acc, sort = false) {
         const type = move > 0 ? 'deposit' : 'withdrawal';
 
         const date = new Date(acc.movementsDates[i]);
-        const day = `${date.getDate()}`.padStart(2, 0);
-        const month = `${date.getMonth() + 1}`.padStart(2, 0);
-        const year = date.getFullYear();
-        const displayDate = `${day}/${month}/${year}`;
+        const displayDate = formatMovementDate(date);
 
         const html = `
         <div class="movements__row">
